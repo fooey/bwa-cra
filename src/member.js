17980d19a92b5b6613cdd13fc4869ba663c42540
Jason Rushton <jasonr@busybusy.com>
2018-06-14 11:06:15 -0600

    add some buttons

--- src/member.js
+++ src/member.js
@@ -42,15 +42,34 @@ const Member = ({ match }) => {
 
 				return (
 					<Fragment>
+						<div>
+							<Link className="btn waves-effect waves-light blue" to='/'>
+								<i className="material-icons left">chevron_left</i>
+								<span>Employees Listing</span>
+								<i className="material-icons right">people</i>
+							</Link>
+						</div>
 						<div className="card">
 							<div className="card-content">
 								<div className="card-title">{member.first_name} {member.last_name}</div>
 								<p>{member.email}</p>
 								{/* <p>{member.id}</p> */}
 							</div>
-							<div className="card-action">
-								<Link to='/'>back</Link>
-								<Link to='/'>Clock In</Link>
+							<div className="card-action grey lighten-4">
+								<button className="btn waves-effect waves-light green" type="button">
+									<i className="material-icons left">timer</i>
+									<span>Clock In</span>
+								</button>
+								{' '}
+								<button className="btn waves-effect waves-light amber" type="button">
+									<i className="material-icons left">av_timer</i>
+									<span>Break</span>
+								</button>
+								{' '}
+								<button className="btn waves-effect waves-light red" type="button">
+									<i className="material-icons left">timer_off</i>
+									<span>Clock Out</span>
+								</button>
 							</div>
 						</div>
 
